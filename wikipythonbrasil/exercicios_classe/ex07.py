337d80fcd71035a4b1938f4ce704e28b61625567
kimzera <adrianohkim@gmail.com>
2022-11-17 23:41:14 -0300

    ex07 from class exercises(wiki python brazil) started.

---
+++ wikipythonbrasil/exercicios_classe/ex07.py
@@ -0,0 +1,13 @@
+""" 7.Classe Bichinho Virtual:Crie uma classe que modele um Tamagushi (Bichinho Eletrônico):
+
+Atributos: Nome, Fome, Saúde e Idade b.
+Métodos: Alterar Nome, Fome, Saúde e Idade;
+Retornar Nome, Fome, Saúde e Idade
+Obs: Existe mais uma informação que devemos levar em consideração, o Humor do nosso tamagushi,
+este humor é uma combinação entre os atributos Fome e Saúde, ou seja, um campo calculado,
+então não devemos criar um atributo para armazenar esta informação por que ela pode ser
+calculada a qualquer momento."""
+
+
+class Tamagochi:
+    
